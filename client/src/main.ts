e86987d5f074304771cf0fc9a1f94d2f05661628
zhoug <90252209+zhougsoft@users.noreply.github.com>
2025-01-01 21:20:17 -0400

    add client predicted input

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,6 +5,11 @@ import characterImage from './assets/character.png'
 
 const SERVER_URL = 'ws://localhost:6969'
 
+// These constants should match the server's constants
+const MAP_WIDTH = 800
+const MAP_HEIGHT = 600
+const MOVEMENT_VELOCITY = 2
+
 interface Player extends Schema {
   x: number
   y: number
@@ -17,7 +22,9 @@ interface State extends Schema {
 class GameScene extends Phaser.Scene {
   client: Client
   room: Room<State> | null = null
+
   playerSprites: { [sessionId: string]: Phaser.GameObjects.Sprite } = {}
+  currentPlayerSprite: Phaser.GameObjects.Sprite | null = null
 
   // User input state
   cursorKeys?: Phaser.Types.Input.Keyboard.CursorKeys
@@ -49,16 +56,26 @@ class GameScene extends Phaser.Scene {
 
     try {
       this.room = await this.client.joinOrCreate('main_room')
-      console.log('joined!')
+      console.log('joined with session id:', this.room.sessionId)
 
-      // Listen for new other players
+      // Listen for players joining the room
       this.room.state.players.onAdd((player, sessionId) => {
+        if (!this.room) {
+          console.error('room not found')
+          return
+        }
+
         // Create a local sprite for the new player & store a reference by sessionId
         const sprite = this.add.sprite(player.x, player.y, 'character')
         sprite.setScale(0.25)
         this.playerSprites[sessionId] = sprite
 
-        // Listen for server updates from this other player
+        // Store a reference for the current player sprite
+        if (sessionId === this.room.sessionId) {
+          this.currentPlayerSprite = sprite
+        }
+
+        // Listen for server updates from the player
         player.onChange(() => {
           // Update local sprite's state with the server's updated data
           sprite.setData('serverX', player.x)
@@ -88,10 +105,17 @@ class GameScene extends Phaser.Scene {
    * Runs once per frame
    */
   update(_time: number, _delta: number): void {
+    // Skip loop if not loaded
+    if (!this.room || !this.currentPlayerSprite) return
+
     // Interpolate all player sprite positions
     for (let sessionId in this.playerSprites) {
+      // Skip interpolation for the current player
+      if (sessionId === this.room.sessionId) continue
+
       const sprite = this.playerSprites[sessionId]
       const { serverX, serverY } = sprite.data.values
+
       sprite.x = Phaser.Math.Linear(sprite.x, serverX, 0.2)
       sprite.y = Phaser.Math.Linear(sprite.y, serverY, 0.2)
     }
@@ -104,15 +128,28 @@ class GameScene extends Phaser.Scene {
     this.playerInput.down = this.cursorKeys.down.isDown
 
     // Send updated local player input to server as payload for movement calculation
-    if (!this.room) return
     this.room.send(0, this.playerInput)
+
+    // Calculate the player movement locally on the client to predict the next location (reduces perceived latency)
+    // WARNING: these movement calculations should match the server's same calculations
+    if (this.playerInput.left) {
+      this.currentPlayerSprite.x -= MOVEMENT_VELOCITY
+    } else if (this.playerInput.right) {
+      this.currentPlayerSprite.x += MOVEMENT_VELOCITY
+    }
+
+    if (this.playerInput.up) {
+      this.currentPlayerSprite.y -= MOVEMENT_VELOCITY
+    } else if (this.playerInput.down) {
+      this.currentPlayerSprite.y += MOVEMENT_VELOCITY
+    }
   }
 }
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: MAP_WIDTH,
+  height: MAP_HEIGHT,
   backgroundColor: '#000',
   pixelArt: true,
   scene: [GameScene],
